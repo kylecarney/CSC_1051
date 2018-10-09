00b422af76aa24f8100f131c5584e7fb9c495d8d
Kyle Carney <kyle.e.carney@gmail.com>
2018-10-09 08:40:26 -0400

    updated csc lab 7

---
+++ Lab07/AllCaps.java
@@ -0,0 +1,38 @@
+//*************************************************************
+//  AllCaps.java       Author: Kyle Carney
+//  Lab 7
+//*************************************************************
+import java.util.Scanner;
+public class AllCaps
+{
+   
+   public static void main (String[] args){
+   String name;
+
+   Scanner scan = new Scanner(System.in); 
+      
+   int vowelCounter = 0; 
+     
+   System.out.print ("Please enter your first name: ");
+   name = scan.next();
+      
+   System.out.print ("Hello...");
+   
+      int n = name.length();
+      name = name.toUpperCase();
+      for (int i = (n-1); i >= 0; i--){
+         System.out.print("\n$" + name.charAt(i) + " ***");
+    
+
+      if (name.charAt(i) == 'A' || name.charAt(i) == 'E' || name.charAt(i) == 'I' || name.charAt(i) == 'O' || name.charAt(i) == 'U'){ 
+         vowelCounter++;
+         }
+         
+}   
+      System.out.print("\nYour name contains " + vowelCounter); 
+      if (vowelCounter == 1)
+          System.out.print(" vowel.");
+      else 
+         System.out.print(" vowels.");  
+ }   
+}
---
+++ Lab07/Box.java
@@ -0,0 +1,27 @@
+//*************************************************************
+//  Box.java       Author: Kyle Carney
+//  Lab 7 
+//*************************************************************
+import java.util.Scanner;
+public class Box
+{
+   
+   public static void main (String[] args){
+   String name;
+   int dimension;
+   Scanner scan = new Scanner(System.in); 
+   
+   //print input for grid dimension   
+   System.out.print ("Input number for grid dimensions: ");
+   //input grid dimension
+   dimension = scan.nextInt();
+   
+final int MAX_ROWS = dimension;
+
+ for (int row = 1; row <= MAX_ROWS; row++)
+ {
+ for (int star = 1; star <= dimension; star++)
+      System.out.print ("*");
+      System.out.println();
+ }
+      }}
